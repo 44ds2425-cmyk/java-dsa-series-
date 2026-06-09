98c4e8d75d023146abbd2eae7c4ff4ab80f3b061
SPARSH CHOUDHARY_0777 <44ds2425@rkgit.edu.in>
2026-06-09 23:14:09 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+//        huujfgikvkvk hf
+//        jijfij
+//        hihjtifkjg
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        System.out.println("byeg uduh");
+    }}
