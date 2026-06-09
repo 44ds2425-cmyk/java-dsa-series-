9a07925e8e17f49fc175795ce78a3286c51e8186
SPARSH CHOUDHARY_0777 <44ds2425@rkgit.edu.in>
2026-06-09 23:14:48 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,19 @@
+public class Variable {
+    public static void main(String[] args) {
+//        int age = 20;
+//        System.out.println(age);
+//        int AGE = 78;
+//        System.out.println(AGE);
+//        byte _g = 56;
+//        System.out.println(_g);
+//        byte $ =89;
+//        System.out.println($);
+        int age1;
+        age1 = 67;
+        System.out.println(age1);
+        age1 =56;
+        System.out.println(age1);
+
+    }
+
+}
