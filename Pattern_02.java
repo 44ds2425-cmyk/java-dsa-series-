7dbf37ff9b3b5f881093423befa51ae0505518be
SPARSH CHOUDHARY_0777 <44ds2425@rkgit.edu.in>
2026-06-14 20:03:33 +0530

    Add files via upload

---
+++ Pattern_02.java
@@ -0,0 +1,22 @@
+public class Pattern_02 {
+    static void main() {
+//         logical any pattern printing problem
+//        step - 1 find the row
+//       step - 2 find the column no and if fixed no the apply direct and they variable no of column the find they formula
+//      step - 3 analyse each row ---> fomula making
+
+        int n =3;
+        for( int row = 1; row<= n; row++){
+//            row printing
+            for(int col = 1; col<= 5; col++){
+//                column printing
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+
+}
